c2cbd28c13e81bcbc899d94a3fdca25038252df6
ozzy <ozzy@cydeo.com>
2022-12-05 14:31:32 -0500

    Spring Rest

---
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,24 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ResponseWrapper;
+import org.springframework.http.ResponseEntity;
+
+public class TaskController {
+
+    public ResponseEntity<ResponseWrapper> getTasks(){}
+
+    public ResponseEntity<ResponseWrapper> getTaskById(){}
+
+    public ResponseEntity<ResponseWrapper> createTask(){}
+
+    public ResponseEntity<ResponseWrapper> deleteTask(){}
+
+    public ResponseEntity<ResponseWrapper> updateTask(){}
+
+    public ResponseEntity<ResponseWrapper> employeePendingTasks(){}
+
+    public ResponseEntity<ResponseWrapper> employeeUpdateTasks(){}
+
+    public ResponseEntity<ResponseWrapper> employeeArchivedTasks(){}
+
+}
